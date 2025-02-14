95876eb33aec27cab1ff9baac17a543ba317bc44
vinithmoduguru <vinith1142@gmail.com>
2025-02-14 15:29:55 +0530

    Added product apis

--- React/vite-project/src/ProductsList.jsx
+++ React/vite-project/src/ProductsList.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
-import { getProducts } from "./api";
+import { getProductCategories, getProducts } from "./api";
 import { ProductCard } from "./ProductCard/ProductCard";
 import { Input } from "./components/ui/input";
+import { Select, SelectTrigger, SelectContent, SelectItem, SelectValue } from "./components/ui/select";
 
 const ProductsList = () => {
   const [products, setProducts] = useState([]);
+  // create another state for storing categories
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("")
   const [search, setSearch] = useState("");
 
   const fetchProductsData = async () => {
@@ -12,15 +16,24 @@ const ProductsList = () => {
     setProducts(data);
   };
 
+  const fetchCategoryData = async () => {
+    // Call the new api which you created
+    const data = await getProductCategories();
+    setCategories(data)
+  }
+
   useEffect(() => {
     fetchProductsData();
+    fetchCategoryData()
   }, []); //Component Did Mount
 
-  console.log(search);
+  // console.log(categories);
 
   const filteredProducts = products.filter((product) => {
     return product.title.includes(search);
   });
+
+  console.log(selectedCategory)
 
   return (
     <>
@@ -33,6 +46,23 @@ const ProductsList = () => {
           setSearch(e.target.value);
         }}
       />
+
+      <Select onValueChange={(selectedVal) => {
+        setSelectedCategory(selectedVal)
+      }}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a category" />
+          <SelectContent>
+            {categories.map((category) => {
+              return (
+                <SelectItem key={category} value={category}>
+                  {category}
+                </SelectItem>
+              );
+            })}
+          </SelectContent>
+        </SelectTrigger>
+      </Select>
 
       <div className="grid grid-cols-5 gap-4 p-2">
         {filteredProducts.map((product) => {
